9c2135df92cb59c417bde9ad2b2281e55a853a87
Karina Ostrovska <karina.ostrovsika@gmail.com>
2023-05-29 20:31:47 +0300

    fix bag

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Bars3CenterLeftIcon, XMarkIcon } from "@heroicons/react/24/solid";
-import Logo from "@/assets/logo.png";
+import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import ActionButton from "@/shared/ActionButton";
